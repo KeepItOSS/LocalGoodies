01ba168cc94cde0bab26d704a5caefbb4e54de7a
Tom-Jank <tom.jank4@gmail.com>
2024-04-04 16:48:09 +0200

    Initial BE

---
+++ lg-backend/src/main/java/com/example/LocalGoodies/LocalGoodiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.LocalGoodies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalGoodiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalGoodiesApplication.class, args);
+	}
+
+}
