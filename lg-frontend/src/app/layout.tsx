f695ba50416c5395b0c3c6606856e9cb4d4d94d7
Tom-Jank <tom.jank4@gmail.com>
2024-04-14 11:57:58 +0200

    feat: add material ui and base theming

--- lg-frontend/src/app/layout.tsx
+++ lg-frontend/src/app/layout.tsx
@@ -2,27 +2,36 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/nav";
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import { Footer } from "@/components/footer";
+import { ThemeProvider, createTheme } from "@mui/material";
+import theme from "./theme";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Local Goodies",
-  description: "Support your local business!",
+    title: "Local Goodies",
+    description: "Support your local business!",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-            <Navbar />
-            {children}
-            <Footer />
-    </body>
-    </html>
-  );
+        children: React.ReactNode;
+    }>) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <AppRouterCacheProvider>
+                    <Navbar />
+                    <main className="flex min-h-screen flex-col items-center">
+                        <ThemeProvider theme={theme}>
+                            {children}
+                        </ThemeProvider>
+                    </main>
+                    <Footer />
+                </AppRouterCacheProvider>
+            </body>
+        </html>
+    );
 }
