86ab80b77cb83585b7aa69914952b0e9dea7f2dc
Tom-Jank <tom.jank4@gmail.com>
2024-04-17 16:33:09 +0200

    fix: typos

--- lg-backend/src/main/java/com/example/LocalGoodies/api/business_management/business_listing/BusinessListingServiceImpl.java
+++ lg-backend/src/main/java/com/example/LocalGoodies/api/business_management/business_listing/BusinessListingServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class BusinessListingServiceImpl implements BusinessListingService{
+public class BusinessListingServiceImpl implements BusinessListingService {
 
     private final BusinessListingRepository businessListingRepository;
 
