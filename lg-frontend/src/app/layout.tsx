f5c0f56aa4d66a58b8afbb1f2be0617c941c4312
Tom-Jank <tom.jank4@gmail.com>
2024-04-29 14:38:56 +0200

    remove usused

--- lg-frontend/src/app/layout.tsx
+++ lg-frontend/src/app/layout.tsx
@@ -2,10 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/nav";
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import { Footer } from "@/components/footer";
-import { ThemeProvider, createTheme } from "@mui/material";
-import theme from "./theme";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,15 +19,11 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={inter.className}>
-                <AppRouterCacheProvider>
-                    <Navbar />
-                    <main className="flex min-h-screen flex-col items-center">
-                        <ThemeProvider theme={theme}>
-                            {children}
-                        </ThemeProvider>
-                    </main>
-                    <Footer />
-                </AppRouterCacheProvider>
+                <Navbar />
+                <main className="flex min-h-screen flex-col items-center">
+                    {children}
+                </main>
+                <Footer />
             </body>
         </html>
     );
--- lg-frontend/src/app/search/http.tsx
+++ lg-frontend/src/app/search/http.tsx
@@ -1,12 +1,12 @@
 import { Business } from "./types";
+import { unstable_noStore as noStore } from 'next/cache';
 
 const BASE_URL = 'http:localhost:8080/api';
 
 export async function getBusinesses(queryParam: string): Promise<Business[]> {
+    noStore();
     try {
-        const response = await fetch(BASE_URL + `/search?type=${queryParam}`, {
-            next: { revalidate: 60 }
-        });
+        const response = await fetch(BASE_URL + `/search?type=${queryParam}`);
         return response.json();
     } catch(error) {
         throw new Error('Failed to fetch data');
@@ -14,10 +14,9 @@ export async function getBusinesses(queryParam: string): Promise<Business[]> {
 }
 
 export async function getAllBusinesses(): Promise<Business[]> {
+    noStore();
     try {
-        const response = await fetch(BASE_URL + '/search/all', {
-            next: { revalidate: 60 }
-        });
+        const response = await fetch(BASE_URL + '/search/all');
         return response.json();
     } catch(error) {
         throw new Error('Failed to fetch data');
